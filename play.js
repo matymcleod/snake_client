3f9f8be84fb2e8b25b5162743950b5d724c0a0f5
matymcleod <matymcleod@gmail.com>
2021-06-30 20:30:28 +0000

    adding client.js file and updated to play.js to connect to remote server and  to listen to messages from server

--- play.js
+++ play.js
@@ -2,12 +2,15 @@ const net = require("net");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: 135.23.223.133,
-    port: 50542
+    host: '135.23.223.133',
+    port: '50542'
   });
 
   conn.setEncoding("utf8");
 
+  conn.on("data", (message) => {
+    console.log(`Message from server: ${message}`);
+  })
   return conn;
 };
 console.log("Connecting...");
