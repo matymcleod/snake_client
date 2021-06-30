014e3630b1f104ed4fd392fc5016c0412f73cd68
matymcleod <matymcleod@gmail.com>
2021-06-30 19:56:32 +0000

    setting up server connection

---
+++ play.js
@@ -0,0 +1,14 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 135.23.223.133,
+    port: 50542
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+console.log("Connecting...");
+connect();
