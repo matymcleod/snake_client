0070dcc24f7f0a2b9c8121ea9152ca8f1b394a11
matymcleod <matymcleod@gmail.com>
2021-06-30 21:49:24 +0000

    added connection status message upon successful connection to server and added my initials

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+const net = require("net");
+
 const connect = function() {
   const conn = net.createConnection({
     host: '135.23.223.133',
@@ -7,11 +9,19 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on("data", (message) => {
-  console.log(`Message from server: ${message}`);
+    console.log(`Message from server: ${message}`);
   })
+
+  conn.on("connect", () => {
+    console.log('You are now connected');
+    conn.write('Name: MAT');
+  });
+
   return conn;
 };
+
 console.log("Connecting...");
+
 connect();
 
 module.exports = {
